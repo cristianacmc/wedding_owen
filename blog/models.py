69656f679e78cc1f5cadc3223a0cdba2db81c8e7
Cristiana Costa <cristianacmc@gmail.com>
2019-01-15 22:22:32 +0000

    Add MDB files

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+	name = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+	Subject = models.CharField(max_length=200)
+	message = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+
+	#def publish(self):
+    #	self.published_date = timezone.now()
+    #	self.save()
+
+	#def __str__(self):
+    #    return self.email
--- mysite/settings.py
+++ mysite/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
